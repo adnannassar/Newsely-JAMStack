d10111ab65b72e26e69448c9381ec9774bbbe2e4
Adnan Nassar <mohamad-adnan.nassar@adesso.de>
2020-09-26 17:04:37 +0200

    ADDED - Text in the Unternehmen News Part

--- src/_data/technologyNews.js
+++ src/_data/technologyNews.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const technologyInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=technology&apiKey=8e39a654c4f04f038d5053f4b06214a6&pageSize=4`;
+const technologyInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=technology&apiKey=8e39a654c4f04f038d5053f4b06214a6&pageSize=5`;
 
 async function getNews ()  {
     try {
