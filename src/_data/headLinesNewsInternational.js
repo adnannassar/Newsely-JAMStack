f5e5ef2ec3d1a0015724db5f30ac496171bcf675
Adnan Nassar <mohamad-adnan.nassar@adesso.de>
2021-01-02 05:43:07 +0100

    ADDED - Dark / Light Theme Added

--- src/_data/headLinesNewsInternational.js
+++ src/_data/headLinesNewsInternational.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 require('dotenv').config();
-const topInternationalHeadLines = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${process.env.API_KEY_2_NEWS}&pageSize=1`;
+const topInternationalHeadLines = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${process.env.API_KEY_1_NEWS}&pageSize=1`;
 
 async function getNews ()  {
     try {
