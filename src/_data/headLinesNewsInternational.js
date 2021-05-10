090ffa73286839fa262b9471ad787d035e972d6c
Adnan Nassar <mohamad-adnan.nassar@adesso.de>
2021-05-10 13:48:17 +0200

    Fixed - Dark / Light Theme

--- src/_data/headLinesNewsInternational.js
+++ src/_data/headLinesNewsInternational.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 require('dotenv').config();
-const topInternationalHeadLines = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${process.env.API_KEY_1_NEWS}&pageSize=1`;
+const topInternationalHeadLines = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${process.env.API_KEY_3_NEWS}&pageSize=1`;
 
 async function getNews ()  {
     try {
--- src/_data/healthNews.js
+++ src/_data/healthNews.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 require('dotenv').config();
-const healthInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=health&apiKey=${process.env.API_KEY_1_NEWS}&pageSize=19`;
+const healthInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=health&apiKey=${process.env.API_KEY_3_NEWS}&pageSize=19`;
 
 async function getNews ()  {
     try {
