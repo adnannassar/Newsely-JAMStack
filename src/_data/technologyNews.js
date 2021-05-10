9773c2c7ee888f0109b92da0a117ce0a565eb563
Adnan Nassar <mohamad-adnan.nassar@adesso.de>
2021-05-10 13:53:41 +0200

    Fixed - api key fix

--- src/_data/technologyNews.js
+++ src/_data/technologyNews.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 require('dotenv').config();
-const technologyInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=technology&apiKey=${process.env.API_KEY_3_NEWS}&pageSize=18`;
+const technologyInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=technology&apiKey=${process.env.API_KEY_1_NEWS}&pageSize=18`;
 
 async function getNews ()  {
     try {
