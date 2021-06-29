c4a2e8546be0b9b21bfb2ab479826701d25227f3
Adnan Nassar <mohamad-adnan.nassar@adesso.de>
2021-06-29 16:42:14 +0200

    Added some fixes and improvements in code

--- src/_data/scienceNewsGermany.js
+++ src/_data/scienceNewsGermany.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 require('dotenv').config();
-const scienceInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=science&apiKey=${process.env.API_KEY_1_NEWS}&pageSize=12`;
+const scienceInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=science&apiKey=${process.env.API_KEY_2_NEWS}&pageSize=12`;
 
 async function getNews ()  {
     try {
